435faaff668226f6dcf788af7cfb7648f1563902
Osama Haddad <ohaddad@flinks.io>
2022-02-15 20:12:50 -0500

    first commit

---
+++ precache-manifest.617c4cf369461e595f2f5f54dd2bfc20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86fbe83910f37a4636b",
+    "url": "/css/app.43e90e6b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7572a2898dd8588ba9cbc80e5f34e4b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86fbe83910f37a4636b",
+    "url": "/js/app.6d58d169.js"
+  },
+  {
+    "revision": "fb71331d57a812928455",
+    "url": "/js/chunk-vendors.6dcb437a.js"
+  },
+  {
+    "revision": "576b2e25fb4a8f4c3c4fd7df99e9fe25",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
